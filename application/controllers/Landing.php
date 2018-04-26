6da2584aa96a89dae7ded63444a28e0fc46cd53f
RakhmaAksataAriela <rakhma.tata921@gmail.com>
2018-04-26 20:16:30 +0700

    Rebase

--- application/controllers/Landing.php
+++ application/controllers/Landing.php
@@ -5,6 +5,6 @@ class Landing extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('landing');
+		$this->load->view('form_user_v');
 	}
 }
---
+++ application/controllers/User.php
@@ -0,0 +1,19 @@
+<?php
+class User extends CI_Controller{
+	public function form(){
+		$this->load->view('form_user_v');
+	}
+
+	public function add(){
+		//untuk mendapatkan data
+		echo $this->input->post("username") .
+		"<br/>";
+		echo $this->input->post("password") .
+		"<br/>";
+		echo $this->input->post("fullname") .
+		"<br/>";
+		echo $this->input->post("level") .
+		"<br/>";
+	}
+}
+?>
